2a6e342ae23ba8c1e964e5a8c90ef0f914a56cc8
brenoleitep <brenoleitepereira@gmail.com>
2024-02-23 20:34:43 -0300

    Fix: improve create company hook

--- src/components/Modals/CreateCompanyModal/useCreateCompanyModal.ts
+++ src/components/Modals/CreateCompanyModal/useCreateCompanyModal.ts
@@ -1,7 +1,9 @@
+import axios from 'axios';
 import { useState } from 'react';
 
 export const useCreateCompanyModal = () => {
   const [open, setOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
     const handleClickOpen = () => {
     setOpen(true);
@@ -10,6 +12,25 @@ export const useCreateCompanyModal = () => {
     const handleClose = () => {
       setOpen(false);
     };
+    
+    const handleSubmitForm = async (data: Record<string, string>) => {
+      try {
+        setIsLoading(true)
+        const token = localStorage.getItem('@userToken');
+        const config = {
+          headers: {
+            Authorization: `${token}`,
+          },
+        };
+        const response = await axios.post('https://api-ambisis.onrender.com/api/company/createCompany', data, config);
+        
+        if(response.status === 201) handleClose();
+      } catch (error) {
+        console.log('Erro de validação:', error);
+      } finally {
+        setIsLoading(false)
+      }
+    };
 
-  return {handleClickOpen, handleClose, open}
+  return {handleClickOpen, handleClose, open, isLoading, handleSubmitForm}
 }
