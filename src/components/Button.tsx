86a58cac18f5a9096950daf992506ed172cd96f0
brenoleitep <brenoleitepereira@gmail.com>
2024-02-23 11:46:25 -0300

    Fix: adjust to improve button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 
 interface ButtonProps {
   text: string;
-  onClick: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
+const Button: React.FC<ButtonProps> = ({ text }) => {
   return (
-    <button onClick={onClick} className='w-full h-[65px] max-w-[343px] bg-primary rounded-lg text-2xl text-white'>
+    <button type='submit' className='w-full h-[65px] max-w-[343px] bg-primary rounded-lg text-2xl text-white'>
       {text}
     </button>
   );
