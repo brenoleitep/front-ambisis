5485a80273784371262a1012d0c32ee4696c8750
brenoleitep <brenoleitepereira@gmail.com>
2024-02-23 11:44:13 -0300

    Chore: remove unnecessary variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,11 +15,7 @@ const config: Config = {
       animation: {
         'spin-slow': 'spin 90s linear infinite',
       },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+
     },
   },
   plugins: [require("daisyui")],
